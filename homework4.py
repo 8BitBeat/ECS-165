788d87818b5b6cd1990dc1b37ec8ab52e32d6e6c
Edmond Wang <edmondmwang@gmail.com>
2016-03-06 19:23:10 -0800

    Added psycopg2 and fixed ending commas

--- homework4.py
+++ homework4.py
@@ -1,7 +1,17 @@
 import csv
+import psycopg2
+import os
 
 count = 0
 prev_msn = ""
+
+conn = psycopg2.connect("dbname=postgres host=/home/" + os.environ['USER'] + "/postgres")
+cur = conn.cursor()
+
+
+#create the tables dude okay
+cur.execute(""" """)
+
 with open('EIA_CO2_Electricity_2015.csv', 'rb') as csvfile:
     reader = csv.reader(csvfile)
     headings = reader.next()
@@ -11,18 +21,17 @@ with open('EIA_CO2_Electricity_2015.csv', 'rb') as csvfile:
     
     for row in reader:
         query+='('+ row[0] +', ' + row[1] + ', ' + row[2] + ' ), ' 
+        #when there is a new MSN we insert into the misc relation
         if(row[0] != prev_msn):
             prev_msn = row[0]
             misc_query+='('+ row[0] +', ' + row[3] + ', ' + row[4] + ', ' + row[5]  +' ), '
-
-
-
         ++count
         if count == 1000: 
-            query = query[:-2]
+            query = query[:-2] + ';'
             #insert query into table
             query = 'INSERT INTO Electricity ('+ headings[0] +', ' + headings[1] + ', ' + headings[2] + ' ) VALUES '
             count = 0
-    misc_query = misc_query[:-2]
+
+    misc_query = misc_query[:-2] + ';'
     #insert misc_query into table        
     print(misc_query)
