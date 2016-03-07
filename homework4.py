795ae6c49b4e30434f664a39458ed627767358ca
Edmond Wang <edmondmwang@gmail.com>
2016-03-07 00:39:08 -0800

    Doesnt work yet
    Didn't include conn.commit

--- homework4.py
+++ homework4.py
@@ -5,18 +5,36 @@ import os
 count = 0
 prev_msn = ""
 
-conn = psycopg2.connect("dbname=postgres host=/home/" + os.environ['USER'] + "/postgres")
-cur = conn.cursor()
+try:
+    conn = psycopg2.connect("dbname=postgres host=/home/" + os.environ['USER'] + "/postgres")
+except:
+    print("didnt work")
 
+try:
+    cur = conn.cursor()
+except:
+    print("didnt work 2")
 
-#create the tables dude okay
-cur.execute(""" """)
+try:
+    cur.execute("CREATE TABLE electricity( MSN varchar(7), YYYYMM int, Value varchar(13));")
+except:
+    print"dun work"
 
-with open('EIA_CO2_Electricity_2015.csv', 'rb') as csvfile:
+with open('EIA_CO2_Electricity_2015.csv', 'r') as csvfile:
+    
+    # try:
+    #     cur.execute("CREATE TABLE electricity( MSN varchar(7), YYYYMM int, Value varchar(13));")
+    # except:
+    #     print "bad execution 1"
+    # try:
+    #     cur.execute("CREATE TABLE electricity_misc(MSN varchar(7), Column_Order varchar(2), Description varchar(55), Unit varchar(39));")
+    # except:
+    #     print "bad exec 2"
+
     reader = csv.reader(csvfile)
     headings = reader.next()
-    query = 'INSERT INTO Electricity ('+ headings[0] +', ' + headings[1] + ', ' + headings[2] + ' ) VALUES '
-    misc_query = 'INSERT INTO ElectricityMisc ('+ headings[0] +', ' + headings[3] + ', ' + headings[4] + ', ' + headings[5] +  ' ) VALUES '
+    query = 'INSERT INTO electricity ('+ headings[0] +', ' + headings[1] + ', ' + headings[2] + ' ) VALUES '
+    misc_query = 'INSERT INTO electricity_misc ('+ headings[0] +', ' + headings[3] + ', ' + headings[4] + ', ' + headings[5] +  ' ) VALUES '
     
     
     for row in reader:
@@ -29,9 +47,19 @@ with open('EIA_CO2_Electricity_2015.csv', 'rb') as csvfile:
         if count == 1000: 
             query = query[:-2] + ';'
             #insert query into table
-            query = 'INSERT INTO Electricity ('+ headings[0] +', ' + headings[1] + ', ' + headings[2] + ' ) VALUES '
+            try:
+                print "hi"
+                #cur.execute(query)
+            except:
+                print "bad exec 3"
+            query = 'INSERT INTO electricity ('+ headings[0] +', ' + headings[1] + ', ' + headings[2] + ' ) VALUES '
             count = 0
-
+    if count > 0:
+        try:
+            print"stupid"
+            #cur.execute(query)
+        except:
+            print "bad exec 4"
     misc_query = misc_query[:-2] + ';'
-    #insert misc_query into table        
-    print(misc_query)
+    #insert misc_query into table     
+    #cur.execute(misc_query)   
