747aa50f0d11b5d4ecfee8ee334a077dae2c923a
edmondmw <edmondmwang@gmail.com>
2016-03-07 02:35:23 -0800

    It wasn't representative
    Created relations and can insert into relation for EIA electricity

--- homework4.py
+++ homework4.py
@@ -5,61 +5,45 @@ import os
 count = 0
 prev_msn = ""
 
-try:
-    conn = psycopg2.connect("dbname=postgres host=/home/" + os.environ['USER'] + "/postgres")
-except:
-    print("didnt work")
-
-try:
-    cur = conn.cursor()
-except:
-    print("didnt work 2")
-
-try:
-    cur.execute("CREATE TABLE electricity( MSN varchar(7), YYYYMM int, Value varchar(13));")
-except:
-    print"dun work"
+conn = psycopg2.connect("dbname=postgres host=/home/" + os.environ['USER'] + "/postgres")
+cur = conn.cursor()
 
 with open('EIA_CO2_Electricity_2015.csv', 'r') as csvfile:
-    
-    # try:
-    #     cur.execute("CREATE TABLE electricity( MSN varchar(7), YYYYMM int, Value varchar(13));")
-    # except:
-    #     print "bad execution 1"
-    # try:
-    #     cur.execute("CREATE TABLE electricity_misc(MSN varchar(7), Column_Order varchar(2), Description varchar(55), Unit varchar(39));")
-    # except:
-    #     print "bad exec 2"
-
+    cur.execute("CREATE TABLE electricity( MSN varchar(20), YYYYMM int, Value varchar(13));")    
+    cur.execute("CREATE TABLE electricity_misc(MSN varchar(20), Column_Order varchar(2), Description varchar(100), Unit varchar(39));")
+  
     reader = csv.reader(csvfile)
     headings = reader.next()
-    query = 'INSERT INTO electricity ('+ headings[0] +', ' + headings[1] + ', ' + headings[2] + ' ) VALUES '
-    misc_query = 'INSERT INTO electricity_misc ('+ headings[0] +', ' + headings[3] + ', ' + headings[4] + ', ' + headings[5] +  ' ) VALUES '
-    
+    query = 'INSERT INTO electricity ( MSN, YYYYMM, Value ) VALUES '
+    misc_query = 'INSERT INTO electricity_misc ( MSN, Column_Order, Description, Unit ) VALUES '
     
     for row in reader:
-        query+='('+ row[0] +', ' + row[1] + ', ' + row[2] + ' ), ' 
+        count+=1
+        #replace not available with null
+        if row[2] == 'Not Available':
+            query += "('"+ row[0] +"', " + row[1] + ', NULL ), ' 
+        else:
+            query += "('"+ row[0] +"', " + row[1] + ', ' + row[2] + ' ), ' 
+
         #when there is a new MSN we insert into the misc relation
         if(row[0] != prev_msn):
             prev_msn = row[0]
-            misc_query+='('+ row[0] +', ' + row[3] + ', ' + row[4] + ', ' + row[5]  +' ), '
-        ++count
+            misc_query+="( '"+ row[0] +"', " + row[3] + ", '" + row[4] + "', '" + row[5]  +"' ), "
+
+        #insert every 1000 tuples into the relation
         if count == 1000: 
             query = query[:-2] + ';'
-            #insert query into table
-            try:
-                print "hi"
-                #cur.execute(query)
-            except:
-                print "bad exec 3"
-            query = 'INSERT INTO electricity ('+ headings[0] +', ' + headings[1] + ', ' + headings[2] + ' ) VALUES '
+            cur.execute(query)
+            query = 'INSERT INTO electricity ( '+ headings[0] +', ' + headings[1] + ', ' + headings[2] + ' ) VALUES '
             count = 0
+
+    #insert remaining query into the table
     if count > 0:
-        try:
-            print"stupid"
-            #cur.execute(query)
-        except:
-            print "bad exec 4"
+        query = query[:-2] + ';'
+        cur.execute(query)
+
+    #insert into the misc relation
     misc_query = misc_query[:-2] + ';'
-    #insert misc_query into table     
-    #cur.execute(misc_query)   
+    cur.execute(misc_query)   
+
+conn.commit()
